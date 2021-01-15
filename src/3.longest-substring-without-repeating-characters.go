e6c897c5f40ce214b39ce05e86a7a7a13f43dae3
frluter <frluter@gmail.com>
2021-01-15 15:53:14 +0800

    add lengthOfLongestSubstring solution

---
+++ src/3.longest-substring-without-repeating-characters.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=3 lang=golang
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	left, right := 0, 0
+	// 返回子字符串开始的索引
+	res := 0
+
+	for right < len(s) {
+		str := s[right]
+		right++
+		// 进行窗口内数据的一系列更新
+		windows[str]++
+		// 判断左侧窗口是否要收缩
+		for windows[str] > 1 {
+			d := s[left]
+			left++
+			// 进行窗口内数据的一系列更新
+			windows[d]--
+		}
+		// 在这里更新答案
+		if res < (right - left) {
+			res = right - left
+		}
+	}
+
+	return res
+}
+
+// @lc code=end
+
